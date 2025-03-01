38b2ef51578ad3e692cf024090eb893a29f30a9b
KeKyoko <deman.strus555@gmail.com>
2025-03-01 11:00:14 +0200

    Update sh_medals.lua

--- libs/sh_medals.lua
+++ libs/sh_medals.lua
@@ -5,3 +5,4 @@ PLUGIN.medals["medal"] = Material("materials/medal.png");
 PLUGIN.medals["medal2"] = Material("materials/medal.png");
 PLUGIN.medals["medal3"] = Material("materials/medal.png");
 PLUGIN.medals["medal4"] = Material("materials/medal.png");
+PLUGIN.medals["medal6"] = Material("materials/medal.png");
